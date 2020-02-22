2c9abdb74f9c4b57494ccfefcf6cf518b499e078
jadefr <jade@ice.ufjf.br>
2020-02-22 14:43:24 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- Background -->
+    <div id="bg"></div>
+
+    <!-- Header -->
+    <header>
+      <a href="#">Lorem ipsum</a>
+    </header>
+
+    <main>
+      <section id="card">
+        <ul>
+          <li>
+            <span></span>
+            <strong>Lorem ipsum dolor sit amet</strong>
+          </li>
+          <li>
+            <span></span>
+            <strong>Maiores reiciendis exercitationem</strong>
+          </li>
+          <li>
+            <span></span>
+            <strong>Quasi ipsum atque velit</strong>
+          </li>
+        </ul>
+      </section>
+      <section id="primary">
+        <h1>Distinctio</h1>
+        <p>Totam fuga quis consequuntur ex ad non numquam modi vero nulla, animi deleniti ea in id? Nulla, ex!</p>
+
+        <a href="#">Eveniet minima</a>
+      </section>
+    </main>
+
   </div>
 </template>
 
