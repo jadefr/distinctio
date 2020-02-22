19e889fba0fcaebd82ab9ecdb9745b7ce3a0f508
jadefr <jade@ice.ufjf.br>
2020-02-22 19:56:48 -0300

    Change the mixins file from sass to scss

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 
     <!-- Header -->
     <header>
-      <a href="#">Lorem ipsum</a>
+      <a href="#">Distinctio</a>
     </header>
 
     <main>
@@ -35,26 +35,3 @@
 
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
