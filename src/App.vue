69bdd879c441f3aeddf26f3a347a12344430b027
jadefr <jade@ice.ufjf.br>
2020-02-24 16:53:21 -0300

    Add a view for blog

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,8 @@
 <template>
   <div id="app">
-
-    <!-- Background -->
-    <div id="bg"></div>
-
-    <!-- Content -->
-    <div class="content">
-
-      <!-- Header esta causando margin-top -->
-      <header>
-        <h1>Distinctio</h1>
-      </header>
-
-      <main>
-      <!-- List card -->
-      <section id="card">
-        <ul>
-          <li>
-            <span></span>
-            <strong>Lorem ipsum dolor sit amet</strong>
-          </li>
-          <li>
-            <span></span>
-            <strong>Maiores reiciendis exercitationem</strong>
-          </li>
-          <li>
-            <span></span>
-            <strong>Quasi ipsum atque velit</strong>
-          </li>
-          <li>
-            <span></span>
-            <strong>Soluta tempora tenetur veniam veritatis</strong>
-          </li>
-        </ul>
-      </section>
-
-      <section id="primary">
-        <p>Amet assumenda at aut consectetur consequatur cum deserunt dicta error esse et eveniet expedita impedit incidunt, iste labore laborum laudantium magnam maiores mollitia nam nesciunt officiis perferendis porro qui quia quos reprehenderit rerum unde voluptas voluptates.</p>
-<!--        <p>Totam fuga quis consequuntur ex ad non numquam modi vero nulla, animi deleniti ea in id? Nulla, ex!</p>-->
-      </section>
-      </main>
-
-      <a href="#" class="btn">Eveniet minima</a>
-
-    </div>
-
+      <router-link to="/"></router-link>
+    <b-container>
+      <router-view/>
+    </b-container>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Blog from '../views/Blog.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: Blog
+  }
+]
 
 const router = new VueRouter({
   mode: 'history',
