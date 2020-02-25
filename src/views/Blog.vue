f40d3b328c1306f037994d76ca1877edfaf242ab
jadefr <jade@ice.ufjf.br>
2020-02-25 18:32:51 -0300

    Fix import style issue

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Blog</h1>
+  <div class="blog-view-container">
+  <!-- Border -->
+<!--    <div id="left"></div>-->
+<!--    <div id="right"></div>-->
+<!--    <div id="top"></div>-->
+<!--    <div id="bottom"></div>-->
+
+    <!-- Section A -->
+    <section class="section-a">
+      <h1>Section A</h1>
+    </section>
+    <!-- Section b -->
+    <section class="section-b">
+      <h1>Section B</h1>
+    </section>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+  @import '../assets/sass/views/_blog.scss';
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,13 @@
     <!-- Content -->
     <div class="content">
 
-      <!-- Header esta causando margin-top -->
+      <!-- Header -->
       <header>
         <h1>Distinctio</h1>
       </header>
 
       <main>
+
         <!-- List card -->
         <section id="card">
           <ul>
@@ -35,15 +36,16 @@
           </ul>
         </section>
 
+        <!-- Side Text -->
         <section id="side-text">
           <p>Amet assumenda at aut consectetur consequatur cum deserunt dicta error esse et eveniet expedita impedit incidunt, iste labore laborum laudantium magnam maiores mollitia nam nesciunt officiis perferendis porro qui quia quos reprehenderit rerum unde voluptas voluptates.</p>
           <!--        <p>Totam fuga quis consequuntur ex ad non numquam modi vero nulla, animi deleniti ea in id? Nulla, ex!</p>-->
         </section>
       </main>
 
-      <a href="#" class="btn">Eveniet minima</a>
+      <!-- Button -->
+      <a href="/blog" class="btn">Eveniet minima</a>
 
     </div>
-
   </div>
 </template>
