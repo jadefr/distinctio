36b914e8bd6841e7a16cd29ada506282b584e3d9
jadefr <jade@ice.ufjf.br>
2020-02-27 18:26:52 -0300

    Fix home elements' positions

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -19,9 +19,9 @@
         </a>
       </div>
       <b-nav class="nav justify-content-end" id="nav">
-        <b-nav-item active>Active</b-nav-item>
-        <b-nav-item>Link</b-nav-item>
-        <b-nav-item>Another Link</b-nav-item>
+<!--        <b-nav-item active>Active</b-nav-item>-->
+<!--        <b-nav-item>Link</b-nav-item>-->
+<!--        <b-nav-item>Another Link</b-nav-item>-->
         <b-nav-item disabled>Disabled</b-nav-item>
       </b-nav>
     </div>
